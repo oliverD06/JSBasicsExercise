378a4492e742e2deb051e1dd81cfc0741d5095f6
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2024-01-12 09:48:47 -0600

    Exercise donion rings.

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log(7);
+//Numbers, Strings, Booleans, Null, Undefined
+// +, -, *, \, **, %
+console.log(5 % 2);
+console.log(5 % 4);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "Eye");
+console.log("Oliver");
+console.log("Delaney");
+console.log("Oliver".length + "Delaney".length);
+console.log("Avengers: Endgame"[16]);
+//Bonus
+console.log("/");
+console.log(5 ** 4 - 9);
+console.log("I".length + "Am".length  % "Iron".length - "Man".length);
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+var m = 25
+var c = 4
+var u = m + c
+console.log(u)
